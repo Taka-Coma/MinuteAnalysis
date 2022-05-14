4b4d98447aa65997d1aa796577271237c7169087
Taka-Coma <taka-coma@acm.org>
2022-05-14 22:52:41 +0900

    mat gen

--- src/extractKeyphraseForSpeech.py
+++ src/extractKeyphraseForSpeech.py
@@ -4,6 +4,8 @@ import json
 from glob import glob
 
 import pke
+import numpy as np
+from copy import deepcopy
 
 def main():
 	kp_extractor = pke.unsupervised.MultipartiteRank()
@@ -14,8 +16,11 @@ def main():
 
 		speakers = ['政府', '参考人'] + [speech['speaker'] for speech in doc['meetingRecord'][0]['speechRecord']]
 
-		out = doc.copy()
+		kps = []
+
+		out = deepcopy(doc)
 		out['meetingRecord'][0]['speechRecord'] = []
+
 		for speech in doc['meetingRecord'][0]['speechRecord']:
 			kp_extractor.load_document(
 				input=speech['speech'],
@@ -26,8 +31,23 @@ def main():
 			kp_extractor.candidate_selection()
 			kp_extractor.candidate_weighting()
 
-			speech['keyphrase'] = kp_extractor.get_n_best(n=100),
+			speech['keyphrase'] = [k for k, conf in kp_extractor.get_n_best(n=100)]
 			out['meetingRecord'][0]['speechRecord'].append(speech)
+
+			kps += speech['keyphrase']
+
+		kps = list(set(kps))
+		kp_mat = np.zeros((len(out['meetingRecord'][0]['speechRecord']), len(kps)))
+
+		for i, speech in enumerate(out['meetingRecord'][0]['speechRecord']):
+			kps_in_speech = speech['keyphrase']
+
+			inds = [(i, kps.index(kp)) for kp in kps_in_speech]
+			np.put(kp_mat, inds, [1])
+
+		print(kp_mat)
+
+		return
 
 		with open(f'../keyphrases/{doc["meetingRecord"][0]["issueID"]}.json', 'w') as w:
 			json.dump(out, w)
