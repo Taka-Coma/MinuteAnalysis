029a0f1f8783a05b966406a5e55e30515b13f66b
Taka-Coma <taka-coma@acm.org>
2022-06-17 20:57:49 +0900

    mod

--- qa_matching/trainingDataGen.py
+++ qa_matching/trainingDataGen.py
@@ -2,20 +2,31 @@
 
 import json
 import re
+from glob import glob
+import csv
 
 def main():
-	#data = loadMinute('../minutes/119804319X00920190508.json')
-	data = loadMinute('../minutes/119814319X01220190516.json')
+	header = ('sentence', 'next_sentence', 'annotation')
+	for path in glob('../minutes/*.json'):
+		data = loadMinute(path)
 
-	nameOfHouse = data['meetingRecord'][0]['nameOfHouse']
+		minid = path[path.rfind('/')+1:-5]
 
-	heading = None
-	for speech in data['meetingRecord'][0]['speechRecord'][1:]:
-		txt = speech['speech']
+		out = [header]
+		for speech in data['meetingRecord'][0]['speechRecord'][1:]:
+			txt = speech['speech']
 
-		sents = [sent.strip() for sent in txt.split('\r\n')]
-		print(sents)
-		print('===')
+			sents = [sent.strip() for sent in txt.split('\r\n')]
+			for i in range(1, len(sents)):
+				pair = (sents[i-1], sents[i]) 
+
+				out.append(pair)
+
+			out.append(('===',))
+
+		with open(f'./training_data/{minid}.csv', 'w') as w:
+			writer = csv.writer(w)
+			writer.writerows(out)
 
 def loadMinute(path):
 	with open(path, 'r') as r:
